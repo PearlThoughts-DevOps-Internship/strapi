195fc8c83a75210cae7b4271f6aa4fa4ac8264fd
Saniya Shaikh <107477541+safaira@users.noreply.github.com>
2024-06-26 19:04:01 +0530

    Update user_data_strapi.sh

--- terraform/user_data_strapi.sh
+++ terraform/user_data_strapi.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
+#update package list
 sudo apt update -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chmod 764 ~/.nvm/nvm.sh
-~/.nvm/nvm.sh
+
+# install nodejs 18 & npm
 sudo apt install nodejs -y && sudo apt install -y npm
+
+# update package list and install pm2(nodejs Process Manager 2)
 sudo apt update -y && sudo npm install -g pm2
+
+# go to directory to dowload code
 cd /srv/
+
+# clone strapi app 
 git clone https://github.com/safaira/strapi.git
 cd strapi/
+
+# run application 
 pm2 start npm --name strapi -- run start
+
+#restart pm2 
 sudo pm2 startup systemd
 
 # sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.0.0/bin /home/ubuntu/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
