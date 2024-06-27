c14a11d1c775491dba3fcd05164c132ae75bbca6
Adam1911818 <47249488+Adam1911818@users.noreply.github.com>
2024-06-27 15:17:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Create a security group with descriptive names
 resource "aws_security_group" "strapi_app1" {
-  name        = "strapi-myapplication"
+  name        = "strapi-myapp"
   description = "Security group for Strapi application access"
 
   ingress {
