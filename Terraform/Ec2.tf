c66e1048071dc439d747c24d53cc0f773fb41ac9
maheshryali1122 <159117395+maheshryali1122@users.noreply.github.com>
2024-06-28 15:04:09 +0530

    Update Ec2.tf

--- Terraform/Ec2.tf
+++ Terraform/Ec2.tf
@@ -26,14 +26,31 @@ resource "aws_security_group" "sgforstrapi" {
   }
 
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+  depends_on = [ aws_security_group.sgforstrapi ]
+}
+
 
 resource "aws_instance" "ec2forstrapi" {
-  ami                         = "ami-03c983f9003cb9cd1"
+  ami                         = "data.aws_ami.ubuntu.id"
   availability_zone = "us-west-2a"
-  instance_type               = "t2.medium"
+  instance_type               = var.instance_type
   vpc_security_group_ids      = [aws_security_group.sgforstrapi.id]
   subnet_id                   = aws_subnet.publicsubnet.id
-  key_name                    = "taskspt"
+  key_name                    = var.key_name
   associate_public_ip_address = true
   tags = {
     Name = "ec2forstrapi"
