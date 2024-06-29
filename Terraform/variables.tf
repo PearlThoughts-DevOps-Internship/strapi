3aded357646878e7b8ad926e322ad430bae1f2df
maheshryali1122 <159117395+maheshryali1122@users.noreply.github.com>
2024-06-29 19:19:25 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -5,6 +5,58 @@ variable "region" {
 variable "instance_type" {
   type = string
 }
-variable "number" {
+variable "MAHESHR_HOST" {
   type = string
+  sensitive = true
 }
+variable "MAHESHR_PORT" {
+  type = number
+  sensitive = true
+}
+variable "MAHESHR_APP_KEYS" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_API_TOKEN_SALT" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_ADMIN_JWT_SECRET" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_TRANSFER_TOKEN_SALT" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_DATABASE_CLIENT" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_DATABASE_FILENAME" {
+  type = string
+  sensitive = true
+}
+variable "MAHESHR_JWT_SECRET" {
+  type = string
+  sensitive = true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
