25c0ec2278d8c01251cf2826741f3c185738144c
TheMannu <mannuveerashwan@gmail.com>
2024-07-02 01:06:43 +0530

    output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,13 @@
 
 output "public_ip" {
-  value = aws_instance.app_server.public_ip
+  value = aws_instance.strapi.public_ip
 }
 
 output "public_dns" {
-  value = aws_instance.app_server.public_dns
+  value = aws_instance.strapi.public_dns
 }
 
 
 output "ssh_command_dns" {
-  value = "ssh -i key.pem ubuntu@${aws_instance.app_server.public_dns}"
+  value = "ssh -i Ashwanikey.pem ubuntu@${aws_instance.strapi.public_dns}"
 }
