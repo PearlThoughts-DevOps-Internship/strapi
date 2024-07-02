1f7538041cd677cb0fd71b5a12f78c516318d8e4
TheMannu <mannuveerashwan@gmail.com>
2024-07-02 22:49:09 +0530

    terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -97,7 +97,4 @@ resource "aws_instance" "strapi" {
   }
 }
 
-# Output
-output "instance_ip" {
-  value = aws_instance.strapi.public_ip
-}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,13 @@ output "public_ip" {
   value = aws_instance.strapi.public_ip
 }
 
+output "instance_ip" {
+  value = aws_instance.strapi.public_ip
+} 
+
 output "public_dns" {
   value = aws_instance.strapi.public_dns
 }
-
 
 output "ssh_command_dns" {
   value = "ssh -i Ashwanikey.pem ubuntu@${aws_instance.strapi.public_dns}"
