214d4ccd38aa10c7924fc1a9d93cf09a9990df03
Gowtham Uppalapati <161915120+GowthamUppalapati@users.noreply.github.com>
2024-07-04 15:06:18 +0530

    coomit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ variable "private_key_path" {
 
 # Security Group
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-security-group"
+  name        = "Ug-strapi-security-group"
   description = "Security group for Strapi EC2 instance"
 
   ingress {
