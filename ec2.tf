e36c2cc908ed625abc1994d4ae41304c41d6bb2a
Anand Puligilla <99485155+Nandhuana@users.noreply.github.com>
2024-07-05 21:45:39 +0530

    added pem

--- ec2.tf
+++ ec2.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "ec2-node-server2" {
     connection {
       type = "ssh"
       user = "ubuntu"
-      agent    = true 
+      private_key = "/ec2.pem" 
       host        = self.public_ip
     }
   }
